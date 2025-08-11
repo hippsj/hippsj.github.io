2ca3232f46d25423668cba28cd300a108ba3969c
Kadir Lofca <kadirlofca@outlook.com>
2025-08-10 22:55:44 -0400

    astro config and workflow for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hippsj.github.io',
+    site: 'https://jordinhipps.com',
     integrations: [svelte()],
     vite: {
         plugins: [tailwindcss()],

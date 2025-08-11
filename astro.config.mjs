f33e9bcf802bb5f303d994ae30610cce6b0a0c68
Kadir Lofca <kadirlofca@outlook.com>
2025-08-10 20:06:06 -0400

    set astro config for github domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://hippsj.github.io',
     integrations: [svelte()],
     vite: {
         plugins: [tailwindcss()],
